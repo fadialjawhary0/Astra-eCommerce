a9a94a99d827c41199a541e258440103aff2022f
fadialjohari0 <fadialjohari0@gmail.com>
2025-04-22 21:44:02 +0300

    Tailwind packages fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React, { lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar.jsx';
 
 const Home = lazy(() => import('./views/home/Home.jsx'));
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
       </Routes>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className='text-yellow-500 flex justify-around align-baseline'>ASTRA</div>
+    </nav>
+  );
+};
+
+export default Navbar;
